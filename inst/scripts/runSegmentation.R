b60c1b90f376526ec7d4db352435da6e8a062be3
Rob Scharpf <rscharpf@jhsph.edu>
2010-09-09 19:07:41 -0400

    WGA samples are removed from analysis of chromosome 1.

--- inst/scripts/runSegmentation.R
+++ inst/scripts/runSegmentation.R
@@ -1,14 +1,25 @@
+library(Beaty)
 library(mybase)
 library(DNAcopy)
 library(crlmm)
 outdir <- "/amber1/scratch/rscharpf/beaty_beadStudio"
 if(!file.exists(outdir)) dir.create(outdir)
 ldPath(outdir)
-lrSet <- checkExists(name="lrSet", path=outdir, FUN=getLrSet)
+lrSet <- checkExists("lrSet", .path=outdir, .FUN=getLrSet)
 lrSet$pedId <- who(sampleNames(lrSet))
 if(!exists("batch")) stop("batch variable should be defined in the submit_logR_cbs.R script")
-if(!exists("NN")) stop("batch size variable (NN) should be specified in submitter script")      
-sample.index <- splitIndicesByLength(1:7599, NN)[[batch]]                                 
-rD <- cbs(lrSet, sample.index=sample.index)
-save(rD, file=file.path(outdir, paste("rD_", batch, ".rda", sep="")))
+if(!exists("NN")) stop("batch size variable (NN) should be specified in submitter script")
+sample.index <- splitIndicesByLength(1:7599, NN)[[batch]]
+marker.index <- which(chromosome(lrSet) == 1 & !duplicated(position(lrSet)))
+CNA.object <- CNA(genomdat=as.matrix(logR(lrSet)[marker.index, sample.index]),
+		  chrom=chromosome(lrSet)[marker.index],
+		  maploc=position(lrSet)[marker.index],
+		  data.type="logratio",
+		  sampleid=sampleNames(lrSet)[sample.index])
+smu.object <- smooth.CNA(CNA.object)
+tmp <- segment(smu.object)
+cbs.segs <- print(tmp, showSegRows=TRUE)
+save(cbs.segs, file=file.path(outdir, paste("cbs.segs_", batch, ".rda", sep="")))
+##rD <- cbs(lrSet, sample.index=sample.index)
+##save(rD, file=file.path(outdir, paste("rD_", batch, ".rda", sep="")))
 q("no")
