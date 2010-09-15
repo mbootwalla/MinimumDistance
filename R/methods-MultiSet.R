2054bfeebf80ac69d197db2cf552ec2aa018f2bf
Rob Scharpf <rscharpf@jhsph.edu>
2010-09-15 06:43:36 -0400

    Use a different alpha for segmenting the parent genomes -- make it more sensitive to bumps in the logR

--- R/methods-MultiSet.R
+++ R/methods-MultiSet.R
@@ -1,1 +1,2 @@
 setMethod("baf", "MultiSet", function(object) assayData(object)[["BAF"]])
+setMethod("logR", "MultiSet", function(object) assayData(object)[["logR"]])
--- inst/scripts/runSegmentation.R
+++ inst/scripts/runSegmentation.R
@@ -9,17 +9,35 @@ lrSet <- checkExists("lrSet", .path=outdir, .FUN=getLrSet)
 lrSet$pedId <- who(sampleNames(lrSet))
 if(!exists("batch")) stop("batch variable should be defined in the submit_logR_cbs.R script")
 if(!exists("NN")) stop("batch size variable (NN) should be specified in submitter script")
+
 sample.index <- splitIndicesByLength(1:7599, NN)[[batch]]
-marker.index <- which(chromosome(lrSet) == 1 & !duplicated(position(lrSet)))
-CNA.object <- CNA(genomdat=as.matrix(logR(lrSet)[marker.index, sample.index]),
+parent.index <- sample.index[lrSet$pedId[sample.index]=="father" | lrSet$pedId[sample.index]=="mother"]
+offspring.index <- sample.index[lrSet$pedId[sample.index]=="offspring"]
+
+## parents
+marker.index <- which(chromosome(lrSet) == CHR & !duplicated(position(lrSet)))
+open(logR(lrSet))
+CNA.object <- CNA(genomdat=as.matrix(logR(lrSet)[marker.index, parent.index]),
 		  chrom=chromosome(lrSet)[marker.index],
 		  maploc=position(lrSet)[marker.index],
 		  data.type="logratio",
-		  sampleid=sampleNames(lrSet)[sample.index])
+		  sampleid=sampleNames(lrSet)[parent.index])
 smu.object <- smooth.CNA(CNA.object)
-tmp <- segment(smu.object)
-cbs.segs <- print(tmp, showSegRows=TRUE)
-save(cbs.segs, file=file.path(outdir, paste("cbs.segs_", batch, ".rda", sep="")))
-##rD <- cbs(lrSet, sample.index=sample.index)
-##save(rD, file=file.path(outdir, paste("rD_", batch, ".rda", sep="")))
+tmp <- segment(smu.object, verbose=0, alpha=0.1)
+cbs.segs1 <- cbind(tmp@output, tmp@segRows)
+##cbs.segs1 <- print(tmp, showSegRows=TRUE, fullOutput=TRUE)
+
+## offspring
+CNA.object <- CNA(genomdat=as.matrix(logR(lrSet)[marker.index, offspring.index]),
+		  chrom=chromosome(lrSet)[marker.index],
+		  maploc=position(lrSet)[marker.index],
+		  data.type="logratio",
+		  sampleid=sampleNames(lrSet)[offspring.index])
+smu.object <- smooth.CNA(CNA.object)
+tmp <- segment(smu.object, verbose=0, alpha=0.01)
+cbs.segs2 <- cbind(tmp@output, tmp@segRows)
+##cbs.segs2 <- print(tmp, showSegRows=TRUE, fullOutput=TRUE)
+
+cbs.segs <- rbind(cbs.segs1, cbs.segs2)
+save(cbs.segs, file=file.path(outdir, paste("cbs.segs_chr", CHR, "_batch", batch, ".rda", sep="")))
 q("no")
