cb4932ba813f52d7aabcffe5f971a66bc26f9231
Rob Scharpf <rscharpf@jhsph.edu>
2010-11-16 21:06:57 -0500

    Scripts for running CBS on the minimum of logR_F-logR_offspring, logR_M-logR_offspring

--- inst/scripts/RunMinDistanceSegmentation.R
+++ inst/scripts/RunMinDistanceSegmentation.R
@@ -1,10 +1,15 @@
-\SweaveInput{setup}
-<<segment>>=
-##CHR <- 1
-minDistanceSet <- checkExists("minDistanceSet", .path=ldPath(), .FUN=load)
+library(Beaty)
+library(BeatyExperimentData)
+outdir <- beadstudiodir()
+ldPath(outdir)
+data(minDistanceSet)
+data(bsSet)
+featureData(minDistanceSet) <- featureData(bsSet)
+rm(bsSet); gc()
 j <- splitIndicesByLength(1:ncol(minDistanceSet), BATCHSIZE)[[BATCH]]
 marker.index <- which(chromosome(minDistanceSet) == CHR)
 marker.index <- marker.index[!duplicated(position(minDistanceSet)[marker.index])]
+stopifnot(all(diff(order(chromosome(minDistanceSet)[marker.index], position(minDistanceSet)[marker.index])) >= 0))
 invisible(open(copyNumber(minDistanceSet)))
 CNA.object <- CNA(genomdat=as.matrix(copyNumber(minDistanceSet)[marker.index, j]),
 		  chrom=chromosome(minDistanceSet)[marker.index],
@@ -18,6 +23,3 @@ md.segs <- cbind(tmp$output, tmp$segRows)
 fname <- paste("md.segs_chr", CHR, "_batch", BATCH, ".rda", sep="")
 save(md.segs, file=file.path(ldPath(), fname))
 q("no")
-@
-
-
